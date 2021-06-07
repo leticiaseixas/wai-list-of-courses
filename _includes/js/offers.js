11841e0f2cf07dbc46a1c6061933657182e662ca
Letícia Seixas <leticiaseixasp@gmail.com>
2021-06-07 15:07:43 +0100

    Update offers.js

--- _includes/js/offers.js
+++ _includes/js/offers.js
@@ -1,6 +1,7 @@
 const filterForm = document.querySelector('[data-filter-form]');
 
 if (filterForm) {
+  console.log(filterForm);
   filterForm.addEventListener('change', e => {
     submitForm(filterForm);
   });
@@ -102,41 +103,5 @@ if (options) {
         triggered.removeAttribute('hidden');
       }
     });
-  });
-}
-
-const tabsets = document.querySelectorAll('[data-do-tabs]');
-
-if (tabsets) {
-  [].forEach.call(tabsets, tabset => {
-    
-    const tabs = tabset.querySelectorAll('[data-tab]');
-    const tabpanels = tabset.querySelectorAll('[data-tabpanel]');
-
-    if (tabs) {
-      [].forEach.call(tabs, tab => {
-        tab.addEventListener('click', e => {
-          const relatedPanel = document.getElementById(tab.getAttribute('href').substr(1));
-
-          [].forEach.call(tabpanels, tabpanel => {
-            tabpanel.setAttribute('hidden', 'true');
-          });
-
-          relatedPanel.removeAttribute('hidden');
-          relatedPanel.focus();
-
-          [].forEach.call(tabs, tab => {
-            tab.removeAttribute('data-tab-current', '');
-          });
-
-          tab.setAttribute('data-tab-current', '');
-
-          e.preventDefault();
-        });
-      });
-    }
-
-    tabs[0].setAttribute('data-tab-current', '');
-
   });
 }
