b090fb75a848ee9b8714c6e21a32d09300e1e070
Letícia Seixas <leticiaseixasp@gmail.com>
2021-06-15 10:39:17 +0100

    Update offers.js

--- _includes/js/offers.js
+++ _includes/js/offers.js
@@ -21,118 +21,136 @@ if (filterForm) {
 
   function filterJson(form) {
 
-    var jsonData = JSON.parse('{{ site.data.offers | jsonify}}');
-    console.log("jsonData");
-    console.log(jsonData);
+    var jsonOffers = JSON.parse('{{ site.data.offers | jsonify}}');
+    console.log("Offers:");
+    console.log(jsonOffers);
 
-        var filtersOn = [];
-        var newResults = [];
+    var filtersOn = [];
+    var newResults = [];
 
-        // Getting filters on and filtering offers by label
-        form.querySelectorAll("input[type='checkbox']").forEach(el => {
-          if (el.checked) {
-            var label = form.querySelector("label[for='" + el.id + "']");
-            filtersOn.push(label.innerText);
+    // Getting filters on and filtering offers by label
+    form.querySelectorAll("input[type='checkbox']").forEach(el => {
+      if (el.checked) {
+        var label = form.querySelector("label[for='" + el.id + "']");
+        filtersOn.push(label.innerText);
+      }
+    });
+
+    var searchTest = function (varToSearch, jsonOffers) {
+
+      for (var key in jsonOffers) {
+        if (typeof (jsonOffers[key]) === 'object') {
+          searchTest(filtersOn, jsonOffers[key]);
+        } else {
+          if (varToSearch.includes(jsonOffers[key])) {
+            newResults.push(jsonOffers);
+
           }
-        });
-
-        var searchTest = function (varToSearch, jsonData) {
-
-          for (var key in jsonData) {
-            if (typeof (jsonData[key]) === 'object') {
-              searchTest(filtersOn, jsonData[key]);
-            } else {
-              if (varToSearch.includes(jsonData[key])) {
-                // filter repeated offers      #TODO          
-                newResults.push(jsonData);
-              
-              }
-            }
-          }
-
         }
+      }
 
-        searchTest(filtersOn, jsonData);
+    }
 
-        console.log("Filters:");
-        console.log(filtersOn);
-        console.log("Results");
-        console.log(newResults);
+    searchTest(filtersOn, jsonOffers);
 
-        //rebuild document
+    console.log("Filters:");
+    console.log(filtersOn);
+    console.log("Results:");
+    console.log(newResults);
+
+    //rebuild document
+    rebuildList(newResults);
 
   }
 
-/*
+  function rebuildList(newResults){
 
-  function submitForm(form) {
-    // get status message references
-    const statusBusy = document.querySelector('.status-busy');
-    const statusFailure = document.querySelector('.status-failure');
+      // Create result message container and copy HTML from doc
+      const offersList = document.getElementById('offers-list');
 
+      offersList.querySelectorAll('ARTICLE').forEach(el => {
+        console.log(el.id);
+        if(newResults.find(o => o.id === el.id))
+          console.log(o.id);
 
-    console.log(filtersChecked);
-
-    // Post data using the Fetch API
-     fetch(form.action, {
-        method: form.method
-        //body: new FormData(form)
       })
-      // We turn the response into text as we expect HTML
-      .then(res => res.text())
-  
-      // Let's turn it into an HTML document
-      .then(text => new DOMParser().parseFromString(text, 'text/html'))
-  
-      // Now we have a document to work with let's replace the <form>
-      .then(doc => {
-  
-        // Create result message container and copy HTML from doc
-        const offersList = document.getElementById('offers-list');
-        offersList.innerHTML = doc.body.innerHTML;
-  
-        // Allow focussing this element with JavaScript
-        offersList.tabIndex = -1;
-  
-  
-        // Hide the busy state
-        statusBusy.hidden = true;
-  
-        // Hide error message
-        statusFailure.hidden = true;
-  
-        // Unlock form elements
-        Array.from(form.elements).forEach(field => field.disabled = false);
-      })
-      .catch(err => {
-  
-        // Unlock form elements
-        Array.from(form.elements).forEach(field => field.disabled = false);
-  
-        // Return focus to active element
-        lastActive.focus();
-  
-        // Hide the busy state
-        statusBusy.hidden = true;
-  
-        // Show error message
-        statusFailure.hidden = false;
-  
-        console.log(err);
-      });
-  
-    // Before we disable all the fields, remember the last active field
-    const lastActive = document.activeElement;
-  
-    // Show busy state and move focus to it
-    statusBusy.hidden = false;
-  
-    // Disable all form elements to prevent further input
-    Array.from(form.elements).forEach(field => field.disabled = true);
-  
-    // Make sure connection failure message is hidden
-    statusFailure.hidden = true; 
+
+
+
+
+      offersList.innerHTML = doc.body.innerHTML;    
   }
 
-*/
+  /*
+  
+    function submitForm(form) {
+      // get status message references
+      const statusBusy = document.querySelector('.status-busy');
+      const statusFailure = document.querySelector('.status-failure');
+  
+  
+      console.log(filtersChecked);
+  
+      // Post data using the Fetch API
+       fetch(form.action, {
+          method: form.method
+          //body: new FormData(form)
+        })
+        // We turn the response into text as we expect HTML
+        .then(res => res.text())
+    
+        // Let's turn it into an HTML document
+        .then(text => new DOMParser().parseFromString(text, 'text/html'))
+    
+        // Now we have a document to work with let's replace the <form>
+        .then(doc => {
+    
+          // Create result message container and copy HTML from doc
+          const offersList = document.getElementById('offers-list');
+          offersList.innerHTML = doc.body.innerHTML;
+    
+          // Allow focussing this element with JavaScript
+          offersList.tabIndex = -1;
+    
+    
+          // Hide the busy state
+          statusBusy.hidden = true;
+    
+          // Hide error message
+          statusFailure.hidden = true;
+    
+          // Unlock form elements
+          Array.from(form.elements).forEach(field => field.disabled = false);
+        })
+        .catch(err => {
+    
+          // Unlock form elements
+          Array.from(form.elements).forEach(field => field.disabled = false);
+    
+          // Return focus to active element
+          lastActive.focus();
+    
+          // Hide the busy state
+          statusBusy.hidden = true;
+    
+          // Show error message
+          statusFailure.hidden = false;
+    
+          console.log(err);
+        });
+    
+      // Before we disable all the fields, remember the last active field
+      const lastActive = document.activeElement;
+    
+      // Show busy state and move focus to it
+      statusBusy.hidden = false;
+    
+      // Disable all form elements to prevent further input
+      Array.from(form.elements).forEach(field => field.disabled = true);
+    
+      // Make sure connection failure message is hidden
+      statusFailure.hidden = true; 
+    }
+  
+  */
 }
