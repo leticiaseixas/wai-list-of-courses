77a00461a1ef2e8666b0b49b7ad224d86fd2cbfe
Letícia Seixas <leticiaseixasp@gmail.com>
2021-09-01 19:10:10 +0100

    expand/collapse

--- _includes/js/offers.js
+++ _includes/js/offers.js
@@ -82,6 +82,9 @@ if (filterForm) {
     })
 
     if (filtersOn.length === 0) {
+      document.getElementById("no-offers").hidden = true;
+      document.getElementById("yes-offers").hidden = false;
+      
       document.getElementById("total-offers").innerText =
         "Showing " + newResults.length + " offers";
       hideClearFilters(true);
