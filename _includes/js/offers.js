89246938755e2de01e66d64bbf649a006f0c2596
Letícia Seixas <leticiaseixasp@gmail.com>
2021-09-14 13:34:19 +0100

    Update offers.js

--- _includes/js/offers.js
+++ _includes/js/offers.js
@@ -115,7 +115,7 @@ if (filterForm) {
       attName.innerText = f.filterName + ':';
       listFiltersOnString.appendChild(attName);
       var attValues = document.createElement('dd');
-      if(f.filterId = 'language')
+      if(f.filterId == 'language')
         attValues.innerText = jsonLang[f.filterValues[0]].nativeName;
       else
         attValues.innerText = f.filterValues.toString();
