b77c4bc9841c0ff570d1dfaa640716219412ac4e
Letícia Seixas <leticiaseixasp@gmail.com>
2021-09-16 11:19:56 +0100

    Update offers.js

--- _includes/js/offers.js
+++ _includes/js/offers.js
@@ -2,7 +2,7 @@ const filterForm = document.querySelector('[data-filter-form]');
 const jsonOffers = JSON.parse('{{ site.data.offers | jsonify}}');
 const jsonFilters = JSON.parse('{{site.data.filters | jsonify}}');
 const jsonLang = JSON.parse('{{site.data.lang | jsonify}}');
-const jsonCountry = JSON.parse('{{ site.data.countries}}');
+const jsonCountry = JSON.parse('{{ site.data.countries | escape_once}}');
 
 var offersList = document.getElementById('offers-list');
 var buttonExpandAll = document.getElementById("expand-sections");
