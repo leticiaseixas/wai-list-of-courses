4a9f3e3762ec13b5cbcc382e9afa7c50346dd32e
Letícia Seixas <leticiaseixasp@gmail.com>
2022-02-08 19:09:26 +0000

    Update courses.js

--- _includes/js/courses.js
+++ _includes/js/courses.js
@@ -18,7 +18,6 @@ const jsonLang = JSON.parse('{{site.data.lang | jsonify}}');
 const coursesList = document.getElementById('courses-list');
 
 
-alert(excols);
 
 // if (filterForm && sortForm && search) {
 
